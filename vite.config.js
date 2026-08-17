b0feada33f688d2daaec81dbf766b2f0d257bf81
Bhavya <bhavyabhanderi07@gmail.com>
2026-08-17 10:47:23 +0530

    add Cross-Origin-Opener-Policy

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
     port: 5173,
     open: true,
     headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+      'Cross-Origin-Opener-Policy': 'unsafe-none',
     },
     proxy: {
       '/api/v1': {
